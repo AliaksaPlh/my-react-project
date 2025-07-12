ca523488107bced5fdd2e7bb853720c030c7cc15
Aliaksa <sashvero@gmail.com>
2025-07-12 19:51:40 +0300

    feat: create a Loader component

---
+++ src/components/Loader/Loader.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styles from "./Loader.module.css";
+
+const Loader: React.FC = () => {
+  return (
+    <div className={styles.loaderWrapper}>
+      <div className={styles.spinner}></div>
+      <p className={styles.text}>Loading...</p>
+    </div>
+  );
+};
+
+export default Loader;
