6d75b8a9013ba09c1c59419c157d54c87b37ce11
Aliaksa <sashvero@gmail.com>
2025-07-15 16:24:41 +0300

    refactor: delete unnessosary comments

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import type { ChangeEvent } from 'react';
 import Button from '../Button/Button';
 import Input from '../Input/Input';
@@ -25,24 +24,3 @@ export function SearchBar(props: Props) {
     </div>
   );
 }
-
-//SearxhBar component as a class
-// export class SearchBar extends Component<Props> {
-//   render() {
-//     const { value, onChange, onSearch } = this.props;
-
-//     return (
-//       <div style={{ marginBottom: "20px" }}>
-//         <Input
-//           value={value}
-//           onChange={onChange}
-//           placeholder="Enter FULL! Pokémon name..."
-//           style={{ width: "250px" }}
-//         />
-//         <Button onClick={onSearch} className="regularButton">
-//           Search
-//         </Button>
-//       </div>
-//     );
-//   }
-// }
