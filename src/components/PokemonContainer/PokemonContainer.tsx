89940906cc0984d068df3e423dacd85a483672d6
Aliaksa <sashvero@gmail.com>
2025-07-15 16:28:58 +0300

    fix: fix search results, load per 20 items

--- src/components/PokemonContainer/PokemonContainer.tsx
+++ src/components/PokemonContainer/PokemonContainer.tsx
@@ -90,7 +90,7 @@ export class PokemonContainer extends Component<Props, State> {
 
     try {
       const response = await fetch(
-        `https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0`
+        `https://pokeapi.co/api/v2/pokemon?limit=20&offset=0`
       );
       if (!response.ok)
         throw new Error(`Failed to fetch list. Status: ${response.status}`);
