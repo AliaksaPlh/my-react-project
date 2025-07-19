a14bb71cacc299c8824f3a153d1e0d51e5c8773d
Aliaksa <sashvero@gmail.com>
2025-07-19 20:42:23 +0300

    feat: create test for SearchBar component

---
+++ src/components/SearchBar/SearchBar.test.tsx
@@ -0,0 +1,22 @@
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import '@testing-library/jest-dom';
+import { describe, it, expect, vi } from 'vitest';
+import { SearchBar } from './SearchBar';
+
+describe('SearchBar', () => {
+  it('displays Input and Button', () => {
+    render(<SearchBar value="" onChange={() => {}} onSearch={() => {}} />);
+    expect(
+      screen.getByPlaceholderText(/enter full! pokémon name/i)
+    ).toBeInTheDocument();
+    expect(screen.getByRole('button', { name: /search/i })).toBeInTheDocument();
+  });
+  it('calls onSearch when Search button is clicked', async () => {
+    const handleSearch = vi.fn();
+    render(<SearchBar value="" onChange={() => {}} onSearch={handleSearch} />);
+    const button = screen.getByRole('button', { name: /search/i });
+    await userEvent.click(button);
+    expect(handleSearch).toHaveBeenCalledTimes(1);
+  });
+});
