09f56cea59afcea7b5312972a4b00ab6eb2a294f
Aliaksa <sashvero@gmail.com>
2025-07-27 19:06:51 +0300

    feat: add goHome button for nonExistingRoutes

--- src/components/nonExistingRoutes.tsx
+++ src/components/nonExistingRoutes.tsx
@@ -1,10 +1,30 @@
 import React from 'react';
+import { useNavigate } from 'react-router';
+import Button from './Button/Button';
 
 const NotFound: React.FC = () => {
+  const navigate = useNavigate();
+
+  const handleGoHome = () => {
+    navigate('/');
+  };
   return (
-    <div style={{ textAlign: 'center', padding: '2rem' }}>
+    <div
+      style={{
+        textAlign: 'center',
+        padding: '2rem',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+      }}
+    >
       <h1>404 - Page Not Found</h1>
-      <p>Sorry, the page you are looking for does not exist.</p>
+      <p style={{ width: '25rem', textAlign: 'center' }}>
+        Sorry, the page you are looking for does not exist.
+      </p>
+      <Button onClick={handleGoHome} className="go-home-button">
+        Go Home
+      </Button>
     </div>
   );
 };
