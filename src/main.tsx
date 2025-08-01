0fd5a242637e815a8a609a5358accd94424e6f0b
Aliaksa <sashvero@gmail.com>
2025-08-02 01:57:17 +0300

    fix: router to correct ErrorBoundary diaplay

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,14 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router';
 import './index.css';
-import PokemonContainer from './components/PokemonContainer/PokemonContainer';
 import NotFound from './components/nonExistingRoutes.tsx';
 import EboutMePage from './components/AboutMe.tsx';
+import App from './App.tsx';
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <PokemonContainer />,
+    element: <App />,
   },
   {
     path: '*',
